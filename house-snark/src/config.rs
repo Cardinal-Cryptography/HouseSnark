f24eaa69f94802ba787da1036516cc10d2734580
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-10-21 10:58:42 +0200

    remove unnecessary deps; update clap

--- house-snark/src/config.rs
+++ house-snark/src/config.rs
@@ -26,25 +26,31 @@ pub enum Command {
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Args)]
 pub struct GenerateKeysCmd {
     /// Which relation to work on.
-    #[clap(long, short, arg_enum)]
+    #[clap(long, short, value_enum)]
     pub relation: Relation,
 
     /// Which proving system to use.
-    #[clap(long, short, arg_enum, default_value = "groth16")]
+    #[clap(long, short, value_enum, default_value = "groth16")]
     pub system: ProvingSystem,
 }
 
 #[derive(Clone, Eq, PartialEq, Hash, Debug, Args)]
 pub struct GenerateProofCmd {
     /// Which relation to work on.
-    #[clap(long, short, arg_enum)]
+    #[clap(long, short, value_enum)]
     pub relation: Relation,
 
     /// Which proving system to use.
-    #[clap(long, short, arg_enum, default_value = "groth16")]
+    #[clap(long, short, value_enum, default_value = "groth16")]
     pub system: ProvingSystem,
 
     /// Path to a file containing proving key.
-    #[clap(long, short, parse(from_os_str))]
+    #[clap(long, short)]
     pub proving_key_file: PathBuf,
+}
+
+#[test]
+fn verify_cli() {
+    use clap::CommandFactory;
+    Cli::command().debug_assert()
 }
