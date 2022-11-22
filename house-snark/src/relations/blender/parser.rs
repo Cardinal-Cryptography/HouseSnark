1fe62a585b4f393e38cf095574f1bf66bee217bb
Maciej Żelaszczyk <maciek.zelaszczyk@gmail.com>
2022-11-22 13:10:15 +0100

    Removed TODOs

--- house-snark/src/relations/blender/parser.rs
+++ house-snark/src/relations/blender/parser.rs
@@ -9,7 +9,6 @@ pub fn parse_frontend_note(frontend_note: &str) -> Result<FrontendNote> {
     Ok(note_from_bytes(frontend_note.as_bytes()))
 }
 
-// TODO: Change note_from_bytes.
 pub fn parse_frontend_merkle_root(frontend_merkle_root: &str) -> Result<FrontendMerkleRoot> {
     Ok(note_from_bytes(frontend_merkle_root.as_bytes()))
 }
@@ -27,7 +26,6 @@ fn account_from_bytes(bytes: &[u8]) -> FrontendAccount {
     ]
 }
 
-// TODO: Change note_from_bytes.
 pub fn parse_frontend_merkle_path_single(
     frontend_merkle_path_single: &str,
 ) -> Result<FrontendMerklePathSingle> {
