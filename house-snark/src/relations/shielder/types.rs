d0c339968b718a8c7d44f3f64bcaff7b01ac6b62
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-12-02 17:58:57 +0100

    New type system. Castings.

--- house-snark/src/relations/shielder/types.rs
+++ house-snark/src/relations/shielder/types.rs
@@ -5,7 +5,9 @@ pub(super) type FpVar = ark_r1cs_std::fields::fp::FpVar<CircuitField>;
 /// The circuit lifting for the byte type.
 pub(super) type ByteVar = ark_r1cs_std::uint8::UInt8<CircuitField>;
 
-// Types accepted by the relation constructors.
+/// Underlying type for all notes, hashes, merkle roots etc. Just the result of hashing.
+type Hash = [u64; 4];
+
 pub type FrontendNullifier = u64;
 pub type FrontendTrapdoor = u64;
 pub type FrontendNote = [u64; 4];
@@ -17,7 +19,6 @@ pub type FrontendLeafIndex = u64;
 pub type FrontendAccount = [u8; 32];
 pub type FrontendMerklePathNode = [u64; 4];
 
-// Types used internally by the relations (but still outside circuit environment).
 pub(super) type BackendNullifier = CircuitField;
 pub(super) type BackendTrapdoor = CircuitField;
 pub(super) type BackendNote = CircuitField;
@@ -28,33 +29,97 @@ pub(super) type BackendMerklePath = Vec<CircuitField>;
 pub(super) type BackendLeafIndex = CircuitField;
 pub(super) type BackendAccount = CircuitField;
 
-/*
-This is a setup for using Pedersen hashing (with field element compressing). It would work well, but
-there is a serious problem with keeping/retrieving parameters in the contract. With the window
-parameters defined below, serialized parameters take ~133kB. On the other hand, generating them
-exhausts block capacity.
+// Types accepted by the relation constructors.
+//
+// These are 1-tuple types instead of aliases in order to avoid any mistake and provide proper
+// casting functions.
+pub struct _FrontendNullifier(pub u64);
+pub struct _FrontendTrapdoor(pub u64);
+pub struct _FrontendNote(pub Hash);
+pub struct _FrontendTokenId(pub u16);
+pub struct _FrontendTokenAmount(pub u64);
+pub struct _FrontendMerkleRoot(pub Hash);
+pub struct _FrontendMerklePath(pub Vec<Hash>);
+pub struct _FrontendLeafIndex(pub u64);
+pub struct _FrontendAccount(pub [u8; 32]);
+pub struct _FrontendMerklePathNode(pub Hash);
 
+// Types used internally by the relations (but still outside circuit environment).
+//
+// These are 1-tuple types instead of aliases in order to avoid any mistake and provide proper
+// casting functions.
+pub(super) struct _BackendNullifier(pub CircuitField);
+pub(super) struct _BackendTrapdoor(pub CircuitField);
+pub(super) struct _BackendNote(pub CircuitField);
+pub(super) struct _BackendTokenId(pub CircuitField);
+pub(super) struct _BackendTokenAmount(pub CircuitField);
+pub(super) struct _BackendMerkleRoot(pub CircuitField);
+pub(super) struct _BackendMerklePath(pub Vec<CircuitField>);
+pub(super) struct _BackendLeafIndex(pub CircuitField);
+pub(super) struct _BackendAccount(pub CircuitField);
 
-#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Default)]
-pub struct PedersenWindow;
-// We can hash 8 * 128 = 1024 bits.
-impl pedersen::Window for PedersenWindow {
-    const WINDOW_SIZE: usize = 8;
-    const NUM_WINDOWS: usize = 128;
+mod casting {
+    use ark_ff::BigInteger256;
+
+    use super::*;
+
+    /// Generate casting between `frontend_type` and `backend_type`, where:
+    ///  - `frontend_type` is assumed to be a 1-tuple struct wrapping a primitive integer type on at
+    ///    most 64 bits,
+    ///  - `backend_type` is assumed to be a 1-tuple struct wrapping `CircuitField`
+    macro_rules! cast_integer {
+        ($frontend_type:tt, $backend_type:tt) => {
+            impl From<$frontend_type> for $backend_type {
+                fn from($frontend_type(frontend_value): $frontend_type) -> Self {
+                    Self(CircuitField::new(BigInteger256::from(
+                        frontend_value as u64,
+                    )))
+                }
+            }
+        };
+    }
+
+    /// Generate casting between `frontend_type` and `backend_type`, where:
+    ///  - `frontend_type` is assumed to be a 1-tuple struct wrapping `Hash` type,
+    ///  - `backend_type` is assumed to be a 1-tuple struct wrapping `CircuitField`
+    macro_rules! cast_hash {
+        ($frontend_type:tt, $backend_type:tt) => {
+            impl From<$frontend_type> for $backend_type {
+                fn from($frontend_type(frontend_value): $frontend_type) -> Self {
+                    Self(CircuitField::new(BigInteger256::new(frontend_value)))
+                }
+            }
+        };
+    }
+
+    cast_integer!(_FrontendNullifier, _BackendNullifier);
+    cast_integer!(_FrontendTrapdoor, _BackendTrapdoor);
+    cast_integer!(_FrontendTokenId, _BackendTokenId);
+    cast_integer!(_FrontendTokenAmount, _BackendTokenAmount);
+    cast_integer!(_FrontendLeafIndex, _BackendLeafIndex);
+
+    cast_hash!(_FrontendNote, _BackendNote);
+    cast_hash!(_FrontendMerkleRoot, _BackendMerkleRoot);
+
+    impl From<_FrontendMerklePath> for _BackendMerklePath {
+        fn from(_FrontendMerklePath(frontend_path): _FrontendMerklePath) -> Self {
+            Self(
+                frontend_path
+                    .iter()
+                    .map(|node| _BackendNote::from(_FrontendNote(*node)).0)
+                    .collect(),
+            )
+        }
+    }
+
+    impl From<_FrontendAccount> for _BackendAccount {
+        fn from(_FrontendAccount(frontend_account): _FrontendAccount) -> Self {
+            Self(CircuitField::new(BigInteger256::new([
+                u64::from_le_bytes(frontend_account[0..8].try_into().unwrap()),
+                u64::from_le_bytes(frontend_account[8..16].try_into().unwrap()),
+                u64::from_le_bytes(frontend_account[16..24].try_into().unwrap()),
+                u64::from_le_bytes(frontend_account[24..32].try_into().unwrap()),
+            ])))
+        }
+    }
 }
-
-pub type HashFunction = PedersenCRHCompressor<EdwardsProjective, TECompressor, PedersenWindow>;
-pub type HashFunctionParameters = <HashFunction as CRHTrait>::Parameters;
-pub type Hash = <HashFunction as CRHTrait>::Output;
-
-pub type HashFunctionGadget = PedersenCRHCompressorGadget<
-    EdwardsProjective,
-    TECompressor,
-    PedersenWindow,
-    EdwardsVar,
-    TECompressorGadget,
->;
-pub type HashFunctionParametersVar =
-    <HashFunctionGadget as CRHGadget<HashFunction, CircuitField>>::ParametersVar;
-pub type HashVar = <HashFunctionGadget as CRHGadget<HashFunction, CircuitField>>::OutputVar;
- */
